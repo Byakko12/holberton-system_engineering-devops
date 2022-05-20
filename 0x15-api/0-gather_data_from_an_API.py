709e7e7f027faf0e09d4d714059ed1309485511a
Byakko12 <davidlandg@gmail.com>
2022-05-20 14:21:25 -0500

    ADD - 0 file

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+
+    user_id = argv[1]
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(user_id)).json()
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
+    total_tasks = []
+    for task in todos:
+        if task.get('completed') is True:
+            total_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
+          len(total_tasks), len(todos)))
+    print("\n".join("\t {}".format(task) for task in total_tasks))
