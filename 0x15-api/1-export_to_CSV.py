ae95675dbe123ca1ecc2fc0fe05a3dec1fe1f6d8
Byakko12 <davidlandg@gmail.com>
2022-05-20 15:19:13 -0500

    ident

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""""
+"""extend your Python script to export data in the CSV format"""
 import csv
 import requests
 from sys import argv
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     user = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
     todos = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)).json()
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
+        .format(user_id)).json()
     with open("{}.csv".format(user_id), 'w', newline='') as csvfile:
         task_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
